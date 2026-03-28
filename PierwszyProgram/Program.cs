2bc64833f177dbca4418bce4bf8474898b615977
Mariusz Mol <mariusz980@gmail.com>
2026-03-28 20:32:10 +0100

    koniec zajec 1

---
+++ PierwszyProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Podprogram1();
+        Console.WriteLine(3 + 4);
+        Console.WriteLine($"3 - 4");
+        Console.WriteLine("3" + "4");
+        Console.WriteLine(3.3 + 4);
+    }
+
+    static void Podprogram1()
+    {
+        string szkola = "Szkoła Programowania";
+        Console.WriteLine($"Hello, World! Witaj w {szkola}!");
+        Console.WriteLine("Jak masz na imię?");
+        string ?imie = Console.ReadLine();
+        Console.WriteLine($"Cześć buraku, {imie}!");
+    }
+}
