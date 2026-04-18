5cb59ca58fb56ff8fb2d79f0cae20c1eaafe8259
Mariusz Mol <mariusz980@gmail.com>
2026-04-18 13:31:41 +0200

    dynamic

--- PierwszyProgram/Program.cs
+++ PierwszyProgram/Program.cs
@@ -3,29 +3,10 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // string imie ="Ala ma kota";
-        // Console.WriteLine(imie);
-        // string b = imie;
-        // Console.WriteLine(b);
-
-        // imie = "Ala ma psa";
-        // Console.WriteLine(imie);
-        // Console.WriteLine(b);
-
-        Pudelko a = new Pudelko();
-        a.Wartosc = 5;
-
-        Pudelko b = a;
-        Console.WriteLine(a.Wartosc);
-        Console.WriteLine(b.Wartosc);
-        a.Wartosc = 10;
-        Console.WriteLine(a.Wartosc);   
-        Console.WriteLine(b.Wartosc);
-    }
-
-    class Pudelko
-    {
-        public int Wartosc { get; set; }
+        dynamic zmienna = 5;
+        Console.WriteLine(zmienna);
+        zmienna = "tekst";
+        Console.WriteLine(zmienna);
     }
 
 }
