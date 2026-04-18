32ea4521a31c04e10e8617d6a3f8f70cf9cbd539
Mariusz Mol <mariusz980@gmail.com>
2026-04-18 12:10:32 +0200

    poczatek zajec2

--- PierwszyProgram/Program.cs
+++ PierwszyProgram/Program.cs
@@ -1,12 +1,23 @@
-﻿internal class Program
+﻿using System.Text;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Podprogram1();
-        Console.WriteLine(3 + 4);
-        Console.WriteLine($"3 - 4");
-        Console.WriteLine("3" + "4");
-        Console.WriteLine(3.3 + 4);
+        // Podprogram1();
+        // Console.WriteLine(3 + 4);
+        // Console.WriteLine($"3 - 4");
+        // Console.WriteLine("3" + "4");
+        // Console.WriteLine(3.3 + 4);
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Mam na imię Mariusz i mam ");
+        sb.Append("4");
+        sb.Append(" lata.");
+        Console.WriteLine(sb.ToString());
+        StringBuilder tb = new StringBuilder();
+        tb.Append("Mam na imię Mariusz i mam ");
+        tb.Append("44");
+        tb.Append(" lata.");
+        Console.WriteLine(tb.ToString());
     }
 
     static void Podprogram1()
