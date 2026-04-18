28a347fc67e0922fc82cffed0efc05549db9912f
Mariusz Mol <mariusz980@gmail.com>
2026-04-18 12:44:41 +0200

    Typy wartosciowe

--- PierwszyProgram/Program.cs
+++ PierwszyProgram/Program.cs
@@ -3,29 +3,43 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // Podprogram1();
-        // Console.WriteLine(3 + 4);
-        // Console.WriteLine($"3 - 4");
-        // Console.WriteLine("3" + "4");
-        // Console.WriteLine(3.3 + 4);
-        StringBuilder sb = new StringBuilder();
-        sb.Append("Mam na imię Mariusz i mam ");
-        sb.Append("4");
-        sb.Append(" lata.");
-        Console.WriteLine(sb.ToString());
-        StringBuilder tb = new StringBuilder();
-        tb.Append("Mam na imię Mariusz i mam ");
-        tb.Append("44");
-        tb.Append(" lata.");
-        Console.WriteLine(tb.ToString());
+        Zmienne();
     }
 
-    static void Podprogram1()
+    private static void Zmienne()
     {
-        string szkola = "Szkoła Programowania";
-        Console.WriteLine($"Hello, World! Witaj w {szkola}!");
-        Console.WriteLine("Jak masz na imię?");
-        string ?imie = Console.ReadLine();
-        Console.WriteLine($"Cześć buraku, {imie}!");
+        bool? czyPada=false;
+        Console.WriteLine($"Czy pada? (true/false){czyPada}");
+
+        byte wiek = 25;
+        sbyte temperatura = -5;
+        Console.WriteLine($"Wiek: {wiek}");
+        Console.WriteLine($"Temperatura: {temperatura}");
+        int liczba = -1000;
+        uint dodatniaLiczba = 1000;
+        Console.WriteLine($"Liczba: {dodatniaLiczba}");
+        Console.WriteLine($"Liczba: {liczba}"); 
+        long duzaLiczba = 10000000000;
+        Console.WriteLine($"Duża liczba: {duzaLiczba}");     
+        // 	-9,223,372,036,854,775,808 do 9,223,372,036,854,775,807 
+        ulong bardzoDuzaLiczba = 18446744073709551615;
+        Console.WriteLine($"Bardzo duża liczba: {bardzoDuzaLiczba}");
+        var liczba2 = 450000L;
+        Console.WriteLine($"Zmienna var: {liczba2}");
+        decimal cena = 19.99m;
+        Console.WriteLine($"Cena: {cena}");
+        double pi = 3.14159;
+        Console.WriteLine($"Pi: {pi}");
+        float e = 2.71828f;
+        Console.WriteLine($"E: {e}");
+
+        ushort krotkaLiczba = 100;
+        Console.WriteLine($"Krotka liczba: {krotkaLiczba}");
+        short malaLiczba = -100;
+        Console.WriteLine($"Mała liczba: {malaLiczba}");
+
+        char znak = 'A';
+        Console.WriteLine($"Znak: {znak}");
     }
+
 }
